d02bb1131d84134a9c22074d2e031fe1ec5cf3a4
RodolfoMH <joserodolfomore@gmail.com>
2020-12-13 21:57:08 -0400

    Se edito el nivel 2 y la scena Fin para arreglar problemas con la visualizacion del texto final.

--- Assets/Script/TextController.cs
+++ Assets/Script/TextController.cs
@@ -12,32 +12,43 @@ public class TextController : MonoBehaviour
     public float defaultIntruccionesPosition = -81.0f;
     public float defaultTittlePosition = -81.0f;
 
+    public float defaultHorizontalPosition = 460;
+
+    public float defaultTituloVerticalPosition = 280;
+    public float defaultInstruccionVerticalPosition = 280;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(460 , defaultCredictPosition,0);
-        titulo.transform.position = new Vector3(460 , defaultTittlePosition,0);
-        instrucciones.transform.position = new Vector3(460 , defaultIntruccionesPosition,0);
+
+        defaultHorizontalPosition = transform.position.x;
+        defaultTituloVerticalPosition = titulo.transform.position.y;
+        defaultInstruccionVerticalPosition = instrucciones.transform.position.y;
+
+        transform.position = new Vector3(defaultHorizontalPosition , defaultCredictPosition,0);
+        titulo.transform.position = new Vector3(defaultHorizontalPosition , defaultTittlePosition,0);
+        instrucciones.transform.position = new Vector3(defaultHorizontalPosition , defaultIntruccionesPosition,0);
 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(defaultCredictPosition<1000){
-            transform.position = new Vector3(460 , defaultCredictPosition,0);
+        if(defaultCredictPosition<1500){
+            transform.position = new Vector3(defaultHorizontalPosition , defaultCredictPosition,0);
             defaultCredictPosition = defaultCredictPosition+0.5f;
             
         }
 
-        if(defaultCredictPosition>=130 && defaultTittlePosition<280){
-            titulo.transform.position = new Vector3(460 , defaultTittlePosition,0);
+        if(defaultCredictPosition>=130 && defaultTittlePosition<defaultTituloVerticalPosition){
+            titulo.transform.position = new Vector3(defaultHorizontalPosition , defaultTittlePosition,0);
             defaultTittlePosition = defaultTittlePosition+0.5f;
             
         }
 
-        if(defaultTittlePosition>=130 && defaultIntruccionesPosition<165){
-            instrucciones.transform.position = new Vector3(460 , defaultIntruccionesPosition,0);
+        if(defaultTittlePosition>=130 && defaultIntruccionesPosition<defaultInstruccionVerticalPosition){
+            instrucciones.transform.position = new Vector3(defaultHorizontalPosition , defaultIntruccionesPosition,0);
             defaultIntruccionesPosition = defaultIntruccionesPosition+0.5f;
             
         }
